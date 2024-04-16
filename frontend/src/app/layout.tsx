74d217e2bacf15bc57e4ebd801c0f0c4c6aee2d8
LntAxe <ertusari0@gmail.com>
2024-04-16 15:23:15 +0300

    chore(frontend): Replace toaster with sonner

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
